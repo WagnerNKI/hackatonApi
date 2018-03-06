b1d788ce673749ad592e0392e64ca43118097e59
unknown <wagnwnki@gmail.com>
2018-03-06 19:31:55 -0300

    sem node modules

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.put('/iot', function (req, res) {
 
 app.post('/escolas', function (req, res) {
   res.send('POST request to homepage');
+  
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
